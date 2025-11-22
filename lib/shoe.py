49c26db7810747b1da4036306242c4a0251a9a5e
lewis wanjohi <i.johilewis@gmail.com>
2025-11-22 11:39:48 +0300

    done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        return self._brand
+    @brand.setter
+    def brand(self,value):
+        if isinstance(value,str):
+            self._brand = value
+        else:
+            raise TypeError("brand must be a string")  
+          
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self,value):
+        if isinstance(value,(int,float)):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+        
